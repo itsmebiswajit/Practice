d565676a275dd839bd2935fdbdac4903a7f430c4
itsmebiswajit <126906472+itsmebiswajit@users.noreply.github.com>
2026-05-13 11:19:06 +0530

    Update substring.java

--- substring.java
+++ substring.java
@@ -19,3 +19,38 @@ class Main {
         solve(i,o);
     }
 }
+
+
+//Unique Substring
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class Main {
+    public static void subString(String i, String o, Set<String> set){
+        if(i.length()==0){
+            set.add(o);
+            return;
+        }
+        
+        subString(i.substring(1), o, set);
+        subString(i.substring(1), o+i.charAt(0), set);
+    }
+    
+    public static void main(String[] args) {
+        
+        String i="aabc";
+        String o="";
+        Set <String> set=new HashSet<>();
+
+        
+        
+        subString(i, o, set);
+        
+                
+        for (String element : set) {
+            System.out.println(element);
+        }
+    }
+}
